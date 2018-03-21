ba2d3dae6a75049e7722bd3d044a863b7cd7f893
Jan-E <github@ehrhardt.nl>
2018-03-21 19:26:13 +0100

    Build fips - take 5

--- appveyor/Build.cmd
+++ appveyor/Build.cmd
@@ -8,6 +8,8 @@ set PROCESSOR_ARCHITECTURE=AMD64
 rd \usr\local\ssl /s /q
 rd out32dll /s /q
 rd tmp32dll /s /q
+perl -pi.bak -e "s/pause/echo pause//gi" ms\do_fips.bat
+if exist ms\do_fips.bat.bak del ms\do_fips.bat.bak
 call ms\do_fips.bat %1
 for %%f in (ms\*.mak) do perl -pi.bak -e "s/\/Zi //gi" %%f
 if exist ms\*.mak.bak del ms\*.mak.bak
