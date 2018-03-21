a81c821a2efa5cb1589f027ac221fe5c4ab97545
Jan-E <github@ehrhardt.nl>
2018-03-21 19:33:06 +0100

    Build fips - take 6

--- appveyor/Build.cmd
+++ appveyor/Build.cmd
@@ -5,12 +5,12 @@ cd \OpenSSL-dev\openssl-fips-%2
 :x64
 
 set PROCESSOR_ARCHITECTURE=AMD64
-rd \usr\local\ssl /s /q
-rd out32dll /s /q
-rd tmp32dll /s /q
-perl -pi.bak -e "s/pause/echo pause//gi" ms\do_fips.bat
+if exist \usr\local\ssl rd \usr\local\ssl /s /q
+if exist out32dll rd out32dll /s /q
+if exist tmp32dll rd tmp32dll /s /q
+perl -pi.bak -e "s/pause//gi" ms\do_fips.bat
 if exist ms\do_fips.bat.bak del ms\do_fips.bat.bak
-call ms\do_fips.bat %1
+call ms\do_fips.bat
 for %%f in (ms\*.mak) do perl -pi.bak -e "s/\/Zi //gi" %%f
 if exist ms\*.mak.bak del ms\*.mak.bak
 rd out32dll /s /q
