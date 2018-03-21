4fe5efbd14ef124299a7edf007c01903a90a44ad
Jan-E <github@ehrhardt.nl>
2018-03-21 18:54:57 +0100

    Install - take 6

--- appveyor/Install.cmd
+++ appveyor/Install.cmd
@@ -1,16 +1,17 @@
 @echo off
 
 choco install nasm
-choco install unxutils
+choco install unxutils > nul
 
 mkdir \OpenSSL-dev
 cd \OpenSSL-dev
 
 echo Downloading https://www.openssl.org/source/openssl-%1.tar.gz
 appveyor DownloadFile https://www.openssl.org/source/openssl-%1.tar.gz
-tar xvf openssl-%1.tar.gz
+dir openssl-%1.tar.gz
+C:\cygwin\bin\tar.exe xvf openssl-%1.tar.gz
 
 echo Downloading https://www.openssl.org/source/openssl-fips-%2.tar.gz
 appveyor DownloadFile https://www.openssl.org/source/openssl-fips-%2.tar.gz
-tar xvf openssl-fips-%2.tar.gz
-
+dir openssl-fips-%2.tar.gz
+C:\cygwin\bin\tar.exe xvf openssl-fips-%2.tar.gz
