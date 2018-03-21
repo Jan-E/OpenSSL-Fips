8f6e89162e429fb3179c863e2d39806b6f432095
Jan-E <github@ehrhardt.nl>
2018-03-21 18:47:25 +0100

    Install - take 5

--- appveyor/Install.cmd
+++ appveyor/Install.cmd
@@ -8,7 +8,9 @@ cd \OpenSSL-dev
 
 echo Downloading https://www.openssl.org/source/openssl-%1.tar.gz
 appveyor DownloadFile https://www.openssl.org/source/openssl-%1.tar.gz
+tar xvf openssl-%1.tar.gz
 
 echo Downloading https://www.openssl.org/source/openssl-fips-%2.tar.gz
 appveyor DownloadFile https://www.openssl.org/source/openssl-fips-%2.tar.gz
+tar xvf openssl-fips-%2.tar.gz
 
