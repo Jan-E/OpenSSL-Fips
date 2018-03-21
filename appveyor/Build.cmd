e55ae0c1f1087b80cde67438d0c81c5c1161bfd4
Jan-E <github@ehrhardt.nl>
2018-03-22 00:40:21 +0100

    Build fips - take 13

--- appveyor/Build.cmd
+++ appveyor/Build.cmd
@@ -7,6 +7,7 @@ if "%3"=="x64" goto x64
 
 if "%vstudio%"=="VS2012" call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
 if "%vstudio%"=="VS2013" call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+where cl
 set path=%PATH%;"C:\Program Files\NASM"
 set PROCESSOR_ARCHITECTURE=x86
 if exist \usr\local\ssl rd \usr\local\ssl /s /q
@@ -28,13 +29,14 @@ goto done
 
 if "%vstudio%"=="VS2012" call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86_amd64
 if "%vstudio%"=="VS2013" call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
+where cl
 set PROCESSOR_ARCHITECTURE=AMD64
 if exist \usr\local\ssl rd \usr\local\ssl /s /q
 if exist out32dll rd out32dll /s /q
 if exist tmp32dll rd tmp32dll /s /q
 perl -pi.bak -e "s/pause//gi" ms\do_fips.bat
 if exist ms\do_fips.bat.bak del ms\do_fips.bat.bak
-call ms\do_fips.bat
+call ms\do_fips.bat no-asm
 for %%f in (ms\*.mak) do perl -pi.bak -e "s/\/Zi //gi" %%f
 if exist ms\*.mak.bak del ms\*.mak.bak
 rd out32dll /s /q
