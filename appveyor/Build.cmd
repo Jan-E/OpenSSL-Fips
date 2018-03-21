b8a05b196c698a7ef97e39aac8b2d1b4d06e002b
Jan-E <github@ehrhardt.nl>
2018-03-22 00:53:33 +0100

    Build fips - take 14

--- appveyor/Build.cmd
+++ appveyor/Build.cmd
@@ -16,6 +16,7 @@ if exist tmp32dll rd tmp32dll /s /q
 perl -pi.bak -e "s/pause//gi" ms\do_fips.bat
 if exist ms\do_fips.bat.bak del ms\do_fips.bat.bak
 call ms\do_fips.bat
+nmake -i -f ms\ntdll.mak install clean
 for %%f in (ms\*.mak) do perl -pi.bak -e "s/\/Zi //gi" %%f
 if exist ms\*.mak.bak del ms\*.mak.bak
 rd out32dll /s /q
@@ -37,11 +38,11 @@ if exist tmp32dll rd tmp32dll /s /q
 perl -pi.bak -e "s/pause//gi" ms\do_fips.bat
 if exist ms\do_fips.bat.bak del ms\do_fips.bat.bak
 call ms\do_fips.bat no-asm
+nmake -i -f ms\ntdll.mak install clean
 for %%f in (ms\*.mak) do perl -pi.bak -e "s/\/Zi //gi" %%f
 if exist ms\*.mak.bak del ms\*.mak.bak
 rd out32dll /s /q
 rd tmp32dll /s /q
-nmake -i -f ms\ntdll.mak install clean
 nmake -f ms\ntdll.mak install
 
 :done
