ef3cc5a3d0f1b024ef10a568410d86a0b1790813
Jan-E <github@ehrhardt.nl>
2018-03-22 00:26:18 +0100

    Build fips - take 11

--- appveyor/Build.cmd
+++ appveyor/Build.cmd
@@ -34,7 +34,7 @@ if exist out32dll rd out32dll /s /q
 if exist tmp32dll rd tmp32dll /s /q
 perl -pi.bak -e "s/pause//gi" ms\do_fips.bat
 if exist ms\do_fips.bat.bak del ms\do_fips.bat.bak
-call ms\do_fips.bat
+call ms\do_fips.bat no-asm
 for %%f in (ms\*.mak) do perl -pi.bak -e "s/\/Zi //gi" %%f
 if exist ms\*.mak.bak del ms\*.mak.bak
 rd out32dll /s /q
