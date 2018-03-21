e6d222448edea775d7ef5030160fd414f4b79b7e
Jan-E <github@ehrhardt.nl>
2018-03-22 00:14:14 +0100

    Build fips - take 9

--- appveyor/Build.cmd
+++ appveyor/Build.cmd
@@ -1,8 +1,30 @@
 echo off
 cd \OpenSSL-dev\openssl-fips-%2
 
+if "%3"=="x64" goto x64
+
+:x86
+
+if "%vstudio%"=="VS2012" call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+if "%vstudio%"=="VS2013" call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+set PROCESSOR_ARCHITECTURE=x86
+if exist \usr\local\ssl rd \usr\local\ssl /s /q
+if exist out32dll rd out32dll /s /q
+if exist tmp32dll rd tmp32dll /s /q
+perl -pi.bak -e "s/pause//gi" ms\do_fips.bat
+if exist ms\do_fips.bat.bak del ms\do_fips.bat.bak
+call ms\do_fips.bat
+for %%f in (ms\*.mak) do perl -pi.bak -e "s/\/Zi //gi" %%f
+if exist ms\*.mak.bak del ms\*.mak.bak
+rd out32dll /s /q
+rd tmp32dll /s /q
+nmake -f ms\ntdll.mak install
+
+goto done
+
 :x64
 
+if "%vstudio%"=="VS2012" call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86_amd64
 if "%vstudio%"=="VS2013" call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
 set PROCESSOR_ARCHITECTURE=AMD64
 if exist \usr\local\ssl rd \usr\local\ssl /s /q
@@ -16,3 +38,5 @@ if exist ms\*.mak.bak del ms\*.mak.bak
 rd out32dll /s /q
 rd tmp32dll /s /q
 nmake -f ms\ntdll.mak install
+
+:done
