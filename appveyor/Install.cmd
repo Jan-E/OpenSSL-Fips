c63b5d3f999c08a5af1d570e26b48b4efd7aacab
Jan-E <github@ehrhardt.nl>
2018-03-22 07:24:50 +0100

    Build fips - take 22

--- appveyor/Install.cmd
+++ appveyor/Install.cmd
@@ -13,7 +13,7 @@ echo Extracting openssl-%1 in \OpenSSL-dev\openssl-%1
 C:\cygwin\bin\tar.exe xf openssl-%1.tar.gz
 cd openssl-%1
 if "%1"=="1.0.2n" echo Apply patch https://github.com/openssl/openssl/pull/4870/commits
-if "%1"=="1.0.2n" patch -p1 < \OpenSSL-fips\AppVeyor\callback.patch
+if "%1"=="1.0.2n" C:\cygwin\bin\patch.exe -p1 < \OpenSSL-fips\AppVeyor\callback.patch
 cd ..
 
 echo Downloading https://www.openssl.org/source/openssl-fips-%2.tar.gz
