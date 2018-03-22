95b30a4334ce72cbb59506ea36f394562dbf3bd9
Jan-E <github@ehrhardt.nl>
2018-03-22 01:17:13 +0100

    Build fips - take 16

--- appveyor/Build.cmd
+++ appveyor/Build.cmd
@@ -8,6 +8,7 @@ if "%3"=="x64" goto x64
 if "%vstudio%"=="VS2008" call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
 if "%vstudio%"=="VS2012" call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
 if "%vstudio%"=="VS2013" call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+if "%vstudio%"=="VS2015" call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86 8.1
 where cl
 set path=%PATH%;"C:\Program Files\NASM"
 set PROCESSOR_ARCHITECTURE=x86
@@ -25,6 +26,7 @@ goto done
 if "%vstudio%"=="VS2008" call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86_amd64
 if "%vstudio%"=="VS2012" call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86_amd64
 if "%vstudio%"=="VS2013" call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
+if "%vstudio%"=="VS2015" call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64 8.1
 where cl
 set PROCESSOR_ARCHITECTURE=AMD64
 if exist \usr\local\ssl rd \usr\local\ssl /s /q
@@ -32,6 +34,6 @@ if exist out32dll rd out32dll /s /q
 if exist tmp32dll rd tmp32dll /s /q
 perl -pi.bak -e "s/pause//gi" ms\do_fips.bat
 if exist ms\do_fips.bat.bak del ms\do_fips.bat.bak
-call ms\do_fips.bat
+call ms\do_fips.bat no-asm
 
 :done
